73ec9e16000aaee04da1274cde16f99a5d0d49df
Nitish Kumar <nitishzira6150@gmail.com>
2024-07-22 17:36:36 +0530

    blog

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import config from './config/config'
+
+function App() {
+  console.log(config)
+  return (
+    <>
+      <h1>hor pra</h1>
+    </>
+  )
+}
+
+export default App
