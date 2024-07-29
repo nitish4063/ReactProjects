9e8173d9f0af9b7384d2abb95fe6e96025b47bb8
Nitish Kumar <nitishzira6150@gmail.com>
2024-07-29 23:44:05 +0530

    config redux toolkit

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,13 +1,42 @@
-import './App.css'
-import config from './config/config'
+import "./App.css";
+// import config from './config/config'
+import {Header, Footer} from './components'
+
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import authService from "./appwrite/auth.js";
+import { login, logOut } from "./store/authSlice.js";
 
 function App() {
-  console.log(config)
-  return (
-    <>
-      <h1>hor pra</h1>
-    </>
-  )
+  // console.log(config)
+
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logOut());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div>Loading....</div>
+  );
 }
 
-export default App
+export default App;
