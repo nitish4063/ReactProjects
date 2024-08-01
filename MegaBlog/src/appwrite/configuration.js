cd9079d61e9431ea2ed7fdd1f1a70316b8d79104
Nitish Kumar <nitishzira6150@gmail.com>
2024-08-01 20:01:34 +0530

    mega

--- MegaBlog/src/appwrite/configuration.js
+++ MegaBlog/src/appwrite/configuration.js
@@ -1,5 +1,5 @@
 import config from "../config/config";
-import { Client, ID, Databases, Query } from "appwrite";
+import { Client, ID, Databases, Query, Storage } from "appwrite";
 
 export class Service {
   client = new Client();
