c64f32278ab0b2d7a48f83627b3e80bd3a57de1b
Nitish Kumar <nitishzira6150@gmail.com>
2024-08-01 19:18:43 +0530

    mega

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,23 +1,31 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from './container/Container'
+import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-import Button from './Button'
-import Input from './Input'
+import Button from "./Button";
+import Input from "./Input";
 import Login from "./Login";
 import PostCard from "./PostCard";
 import Signup from "./Signup";
+import RTE from "./RTE";
+import PostForm from "./post-form/PostForm";
+import AuthLayout from './AuthLayout'
+import Select from './Select'
 
 export {
-    Header,
-    Footer,
-    Container,
-    Logo,
-    LogoutBtn,
-    Button,
-    Input,
-    Login,
-    PostCard,
-    Signup,
-}
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  Input,
+  Login,
+  PostCard,
+  Signup,
+  RTE,
+  PostForm,
+  AuthLayout,
+  Select,
+};
